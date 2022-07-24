680cfe1561c6551a14624401b0ce8eeecdb1b31c
Lord-Lava <76536776+Lord-Lava@users.noreply.github.com>
2022-07-25 01:11:41 +0530

    Time: 246 ms (6.01%), Space: 80 MB (12.12%) - LeetHub

--- 72-edit-distance/72-edit-distance.cpp
+++ 72-edit-distance/72-edit-distance.cpp
@@ -4,13 +4,13 @@ class Solution {
         if(i==word1.length() and j==word2.length()){
             return 0;
         }
-        if(dp[i][j]!=-1) return dp[i][j];
         if(i==word1.length()){
             return word2.length()-j;
         }
         if(j==word2.length()){
             return word1.length()-i;
         }
+        if(dp[i][j]!=-1) return dp[i][j];
         int ans;
         if(word1[i]==word2[j]) ans = fun(word1,word2,i+1,j+1);
         else ans = min({1 + fun(word1,word2,i,j+1),1 + fun(word1,word2,i+1,j),1 + fun(word1,word2,i+1,j+1)});
@@ -18,8 +18,6 @@ class Solution {
     }
 public:
     int minDistance(string word1, string word2) {
-        if (word1 == "") return word2.size();
-        if (word2 == "") return word1.size();
         memset(dp,-1,sizeof(dp));
         return fun(word1, word2, 0, 0);      
     }
