75f6d9fb751bdb41bb33de749d43e8aa0be24dba
Lord-Lava <76536776+Lord-Lava@users.noreply.github.com>
2022-07-31 11:01:18 +0530

    Time: 177 ms (100.00%), Space: 56.5 MB (100.00%) - LeetHub

--- 2358-maximum-number-of-groups-entering-a-competition/2358-maximum-number-of-groups-entering-a-competition.cpp
+++ 2358-maximum-number-of-groups-entering-a-competition/2358-maximum-number-of-groups-entering-a-competition.cpp
@@ -2,11 +2,9 @@ class Solution {
 public:
     int maximumGroups(vector<int>& grades) {
         int n = grades.size();
-//         sort(grades.begin(), grades.end());
         
-//         for(auto it:grades)cout<<it<<" ";cout<<endl;
+        //if(n<3)return 1;
         
-        if(n<3)return 1;
         int sum = 0;
         int j =1;
         int ans = 0;
@@ -14,24 +12,7 @@ public:
             sum+=j;
             j++;
             ans++;
-        }
-        // cout<<j<<" "<<sum<<endl;
-        // j--;
-        // int ans = j;
-        // sum-=j;
-        // int rem = n-sum;
-        // int s = 0;
-        // for(int i=0;i<rem;i++){
-        //     s+=grades[n-i-1];
-        // }
-        // j--;
-        // int s1=0;
-        // for(int k = ;k<j;k--){
-        //     s1+=grades[n-k-1];
-        // }
-        // if(s1>=s){
-        //     ans--;
-        // }
+        }     
         
         return ans-1;
     }
