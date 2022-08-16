6017a5743e6b945c25280e478470ee732f893e36
Lord-Lava <76536776+Lord-Lava@users.noreply.github.com>
2022-08-16 23:45:51 +0530

    Time: 154 ms (56.85%), Space: 159.6 MB (25.45%) - LeetHub

--- 5-longest-palindromic-substring/5-longest-palindromic-substring.cpp
+++ 5-longest-palindromic-substring/5-longest-palindromic-substring.cpp
@@ -15,7 +15,7 @@ public:
             int len1 = expandFromMiddle(s, i, i);
             int len2 = expandFromMiddle(s, i, i+1);
             int len = max(len1, len2);
-            if(len > end - start + 1) {
+            if(len > end - start) {
                 start = i - ((len-1) / 2);
                 end = i + len/2;
             }
