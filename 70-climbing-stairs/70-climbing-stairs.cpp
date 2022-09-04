712d9a0329a2ae7a99b113c8474e0d28940684a9
Lord-Lava <76536776+Lord-Lava@users.noreply.github.com>
2022-09-04 21:34:06 +0530

    Time: 0 ms (100.00%), Space: 5.8 MB (83.43%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.cpp
+++ 70-climbing-stairs/70-climbing-stairs.cpp
@@ -1,14 +1,23 @@
 class Solution {
     int dp[101];
-    int rec(int n) {
-        if(n<0) return 0;
-        if(n==0) return 1;
-        if(dp[n] != -1) return dp[n];
-        return dp[n] = rec(n-1) + rec(n-2);
+    int rec(int level, int n) {
+        if(level > n) return 0;
+        if(level == n) {
+            return 1;
+        }
+        if(dp[level] != -1) return dp[level];
+        int ans = 0;
+        for(int step =1;step<=2;step++) {
+            if(level+step <= n) {
+                int ways = rec(level+step,n);
+                ans+=ways;
+            }
+        }
+        return dp[level] = ans;
     }
 public:
     int climbStairs(int n) {
         memset(dp,-1,sizeof(dp));
-        return rec(n);
+        return rec(0,n);
     }
 };
