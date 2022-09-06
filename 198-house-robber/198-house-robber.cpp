2613013f34590b62dff892a8ff07e222c8fcae16
Lord-Lava <76536776+Lord-Lava@users.noreply.github.com>
2022-09-06 13:16:37 +0530

    Time: 5 ms (23.70%), Space: 7.7 MB (49.18%) - LeetHub

--- 198-house-robber/198-house-robber.cpp
+++ 198-house-robber/198-house-robber.cpp
@@ -1,24 +1,23 @@
 class Solution {
-    int dp[102][101];
+    int dp[102];
     const int OFFSET = 2;
-    int rec(int level, int lastLooted, vector<int>&nums) {
+    int rec(int level, vector<int>&nums) {
         //base case
         if(level >= nums.size()) return 0;
         //cache check
-        if(dp[level][lastLooted+OFFSET] != -1) return dp[level][lastLooted+OFFSET];
+        if(dp[level] != -1) return dp[level];
         //compute / choice
         int ans = 0;
         //option 1: loot
-        if(lastLooted!=level-1)
-            ans = max(ans,nums[level] + rec(level+1, level, nums));
+        ans = max(ans,nums[level] + rec(level+2, nums));
         //option 2: not loot
-            ans = max(ans,rec(level+1, lastLooted, nums));
+        ans = max(ans,rec(level+1, nums));
         //save and return 
-        return dp[level][lastLooted+OFFSET] = ans;
+        return dp[level] = ans;
     }
 public:
     int rob(vector<int>& nums) {
         memset(dp,-1,sizeof(dp));
-        return rec(0,-2,nums);
+        return rec(0,nums);
     }
 };
