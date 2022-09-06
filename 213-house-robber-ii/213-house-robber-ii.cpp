746ed8d061ac2bb9e11cce908315dd28e82d36bc
Lord-Lava <76536776+Lord-Lava@users.noreply.github.com>
2022-09-06 14:47:32 +0530

    Time: 4 ms (38.63%), Space: 7.8 MB (86.14%) - LeetHub

--- 213-house-robber-ii/213-house-robber-ii.cpp
+++ 213-house-robber-ii/213-house-robber-ii.cpp
@@ -2,8 +2,6 @@ class Solution {
     int dp[102];
     int n;
     int rec(int level, int n, vector<int>&nums) {
-        // cout<<level<<" "<<n<<endl;
-        
         //base case
         if(level >= n) return 0;
         //cache check
@@ -12,11 +10,8 @@ class Solution {
         int ans = 0;
         //option 1: loot
         ans = max(ans,nums[level] + rec((level+2), n, nums));
-        cout<<ans<<endl;
         //option 2: not loot
         ans = max(ans,rec((level+1), n, nums));
-        // cout<<ans<<endl;
-        
         //save and return
         return dp[level] = ans;
     }
@@ -25,7 +20,6 @@ public:
         if(nums.size()==1) return nums[0];
         memset(dp,-1,sizeof(dp));
         int ans1 = rec(0,(int)nums.size()-1,nums);
-        // cout<<ans1<<endl;
         memset(dp,-1,sizeof(dp));
         int ans2 = rec(1,nums.size(),nums);
         return max(ans1,ans2);
